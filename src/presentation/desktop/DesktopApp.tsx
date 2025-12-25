4aab04a6dd73c20ce2b77c9d50664b10d8d60284
Wanlee199 <qu4nl3@gmail.com>
2025-12-26 01:28:22 +0700

    fixing handleSubmit

--- src/presentation/desktop/DesktopApp.tsx
+++ src/presentation/desktop/DesktopApp.tsx
@@ -165,9 +165,9 @@ export const DesktopApp: React.FC = () => {
   //       body: JSON.stringify(data)
   //     });
 
-  //     const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID || '';
-  //     const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID || '';
-  //     const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY || '';
+  //     const serviceId = import.meta.env.EMAILJS_SERVICE_ID || '';
+  //     const templateId = import.meta.env.EMAILJS_TEMPLATE_ID || '';
+  //     const publicKey = import.meta.env.EMAILJS_PUBLIC_KEY || '';
 
   //     if (serviceId && templateId && publicKey) {
   //       await emailjs.send(
