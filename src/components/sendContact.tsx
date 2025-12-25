fc583af8712756490688902386a95aac1716f90d
Wanlee199 <qu4nl3@gmail.com>
2025-12-26 02:05:56 +0700

    refactor: remove unused variable from funcWindow

--- src/components/sendContact.tsx
+++ src/components/sendContact.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 
 const siteKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
 export default async function funcWindow() {
-const [isClient, setClient] = useState(false)
+const [, setClient] = useState(false)
 
   useEffect(() => {
     setClient(true)
