bb067496177be7a093c78515444636c5d9e3f3bc
Wanlee199 <qu4nl3@gmail.com>
2026-01-05 14:29:56 +0700

    delete recaptcha of all page

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <GoogleReCaptchaProvider reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}>
-        <App />
-      </GoogleReCaptchaProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
